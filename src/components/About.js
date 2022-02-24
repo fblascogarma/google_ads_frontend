486ffafd768e438854ec46e3bf5d0ad6c5c06f5c
Francisco Blasco Garma <fblascogarma@gmail.com>
2022-02-24 08:30:41 -0800

    edited about page.

--- src/components/About.js
+++ src/components/About.js
@@ -19,19 +19,23 @@ export const About = () => {
         <div className="container mt-4">
         <br/>
         <h4 className="display-4 text-left mb-4" font="gotham-rounded-bold" style={{color:'rgb(248,172,6)', fontSize:'40px'}}>
-            About FranAds
+            About Fran Ads
         </h4>
         <br />
         <p className="mb-0" font="gotham-rounded-bold" style={{color:'black', fontSize:'20px'}}>
-        FranAds is a <strong style={{color:'rgb(248,172,6)'}} >web app designed and built by Francisco Blasco</strong> to learn
-        about <strong style={{color:'rgb(248,172,6)'}} >Google Ads API</strong>.
+        Fran Ads is a <strong style={{color:'rgb(248,172,6)'}} >web app designed and built by Francisco Blasco</strong> that is integrated with <strong style={{color:'rgb(248,172,6)'}} >Google Ads</strong> and Google My Business.
         <br/>
         <br/>
         It is an SPA (single-page application) that uses Django for the backend, React for the frontend, and Bootstrap for CSS.
         <br />
         <br />
-        The idea is to use this web app to better help Google's partners build great Google Ads solutions that 
-        people love.
+        The goal of this project is to accelerate the Google Ads integration process, and decrease associated development costs. 
+        <br/>
+        <br/>
+        Companies can use Fran Ads as an SDK to begin working with elements within the Google Ads API, 
+        and serve as a guidance system for integrating with Google.
+ 
+
         </p>
         <br />
         <br />
